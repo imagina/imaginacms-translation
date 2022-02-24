116db070fb4f99cd67385f35cc725b7fb8f7f780
msolano <msolano@imaginacolombia.com>
2022-02-24 14:28:50 -0500

    +Add: config `cmsPages` to provider

--- Providers/TranslationServiceProvider.php
+++ Providers/TranslationServiceProvider.php
@@ -28,14 +28,14 @@ use Modules\Translation\Services\TranslationLoader;
 class TranslationServiceProvider extends ServiceProvider
 {
   use CanPublishConfiguration, CanGetSidebarClassForModule;
-  
+
   /**
    * Indicates if loading of the provider is deferred.
    *
    * @var bool
    */
   protected $defer = false;
-  
+
   /**
    * Register the service provider.
    *
@@ -45,42 +45,43 @@ class TranslationServiceProvider extends ServiceProvider
   {
     $this->registerBindings();
     $this->registerConsoleCommands();
-    
+
     view()->composer('translation::admin.translations.index', CurrentUserViewComposer::class);
-    
+
     $this->app['events']->listen(
       BuildingSidebar::class,
       $this->getSidebarClassForModule('translation', RegisterTranslationSidebar::class)
     );
-    
+
     $this->app['events']->listen(LoadingBackendTranslations::class, function (LoadingBackendTranslations $event) {
       $event->load('translations', Arr::dot(trans('translation::translations')));
       $event->load('locales', Arr::dot(trans('translation::locales')));
     });
-    
+
     app('router')->bind('translations', function ($id) {
       return TranslationTranslation::find($id);
     });
   }
-  
+
   public function boot()
   {
     $this->publishConfig('translation', 'config');
-    
+
     $this->mergeConfigFrom($this->getModuleConfigFilePath('translation', 'permissions'), "asgard.translation.permissions");
-    
+    $this->mergeConfigFrom($this->getModuleConfigFilePath('translation', 'cmsPages'), "asgard.translation.cmsPages");
+
     $this->registerValidators();
     $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-    
+
     if ($this->app->runningInConsole() === true) {
       return;
     }
-    
+
     if ($this->shouldRegisterCustomTranslator()) {
       $this->registerCustomTranslator();
     }
   }
-  
+
   /**
    * Should we register the Custom Translator?
    * @return bool
@@ -90,18 +91,18 @@ class TranslationServiceProvider extends ServiceProvider
     if (false === config('app.translations-gui', true)) {
       return false;
     }
-    
+
     if (false === env('INSTALLED', false)) {
       return false;
     }
-    
+
     if (false === Schema::hasTable((new Translation)->getTable())) {
       return false;
     }
-    
+
     return true;
   }
-  
+
   /**
    * Get the services provided by the provider.
    *
@@ -111,36 +112,36 @@ class TranslationServiceProvider extends ServiceProvider
   {
     return [];
   }
-  
+
   private function registerBindings()
   {
     $this->app->bind(TranslationRepository::class, function () {
       $repository = new EloquentTranslationRepository(new Translation());
-      
+
       return new CacheTranslationDecorator($repository);
     });
-    
+
     $this->app->bind(FileTranslationRepository::class, function ($app) {
       return new FileDiskTranslationRepository($app['files'], $app['translation.loader']);
     });
-    
+
     $this->app->bind(
       LocaleRepository::class,
       function () {
         $repository = new EloquentLocaleRepository();
-        
+
         return new CacheLocaleDecorator($repository);
       }
     );
   }
-  
+
   private function registerConsoleCommands()
   {
     $this->commands([
       BuildTranslationsCacheCommand::class,
     ]);
   }
-  
+
   protected function registerCustomTranslator()
   {
     $this->app->singleton('translation.loader', function ($app) {
@@ -148,23 +149,23 @@ class TranslationServiceProvider extends ServiceProvider
     });
     $this->app->singleton('translator', function ($app) {
       $loader = $app['translation.loader'];
-      
+
       $locale = $app['config']['app.locale'];
-      
+
       $trans = new \Illuminate\Translation\Translator($loader, $locale);
-      
+
       $trans->setFallback($app['config']['app.fallback_locale']);
-      
+
       return $trans;
     });
   }
-  
+
   private function registerValidators()
   {
     Validator::extend('extensions', function ($attribute, $value, $parameters) {
       return in_array($value->getClientOriginalExtension(), $parameters);
     });
-    
+
     Validator::replacer('extensions', function ($message, $attribute, $rule, $parameters) {
       return str_replace([':attribute', ':values'], [$attribute, implode(',', $parameters)], $message);
     });
